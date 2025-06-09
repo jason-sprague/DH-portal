fa4b18729bb9bc5d36dbc8aba9ad85add04e69cc
jasprague <leftyjds@gmail.com>
2025-06-09 11:41:08 -0400

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Auth0Provider } from '@auth0/nextjs-auth0';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Auth0Provider>
         {children}
+        </Auth0Provider>
       </body>
     </html>
   );
